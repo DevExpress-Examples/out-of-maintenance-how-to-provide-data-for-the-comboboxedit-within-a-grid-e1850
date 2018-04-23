693a751b207a2fedfe721814311795aa50bfdc77
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace BindComboBoxEdit {
+
+    public partial class Window1 : Window {
+
+        public Window1() {
+            InitializeComponent();
+
+            List<TestData> list = new List<TestData>();
+            for (int i = 0; i < 100; i++) {
+                list.Add(new TestData() { Text = "row " + i, DetailId = i % 5, 
+                    DetailSource = GetDetailSource(i % 5) });
+            }
+
+            grid.DataSource = list;
+        }
+
+        IList<DetailData> GetDetailSource(int i) {
+            List<DetailData> detailSource = new List<DetailData>();
+            for (int j = 0; j < 10; j++) {
+                detailSource.Add(new DetailData() { Id = i + j, 
+                    Value = "value " + (i + j) + " kind  " + i });
+            }
+            return detailSource;
+        }
+    }
+
+    public class TestData {
+        public string Text { get; set; }
+        public int DetailId { get; set; }
+        public object DetailSource { get; set; }
+    }
+
+    public class DetailData {
+        public int Id { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to provide data for the ComboBoxEdit within a grid
+
+
+<p>The following example illustrates how to provide data for ComboBoxEdit according to a row where it's located.</p>
+
+<br/>
+
+
